1a7c257a283c3b12211199f83237f69df1462c20
Florencia Molle <floorimolle98@gmail.com>
2023-04-08 18:29:33 -0300

    Practica con do while e if loop

---
+++ Scripts/compra.js
@@ -0,0 +1,17 @@
+function compra() {
+    let nombre = prompt("Buen día, por favor ingresa tu nombre a continuación");
+    let eleccion = prompt(nombre + " por favor, ingresa el numero de producto del 1 al 9");
+    if (eleccion > 9 || eleccion <= 0) {
+        alert("Ingresaste un número incorrecto");
+    } else {
+        do {
+          alert("El producto número " + eleccion + " fue agregado al carrito.");
+          eleccion = prompt(nombre + " por favor, ingrese otro numero o escriba Exit para finalizar.");
+          if (eleccion == "Exit") {
+            alert("Se agregaron todos los productos al carrito.");
+            break;
+          }
+        } while (eleccion <= 9 && eleccion > 0 && eleccion != null);
+         alert("Ingresaste un número incorrecto");
+    }
+}
