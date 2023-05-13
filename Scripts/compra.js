aab192969eaf1f348c3162cf85080932a5305f21
Florencia Molle <floorimolle98@gmail.com>
2023-05-13 19:42:49 -0300

    Se agrego el boton para vaciar el carrito

--- Scripts/compra.js
+++ Scripts/compra.js
@@ -1,3 +1,4 @@
+//Crea el html para el carrito de compras
 let carritoHTML = document.getElementById("carrito");
 carritoHTML.innerHTML = "<p>Tu carrito</p><div></div>";
 document.querySelector("#tiendaFanPage_Fila header #navbar_Extended-Left").append(carritoHTML);
@@ -22,7 +23,8 @@ class Producto {
   }
 }
 
-// Array y funcion que agrega los productos seleccionados a dicho array
+// Array y funcion que agrega los productos seleccionados a dicho array y tambien al html creado para el carrito
+// ademas suma el iva al precio
 const productos = [];
 function addToCart(codigo, nombre, id, precio) {
   productos.push(new Producto(codigo, nombre, id, precio));
@@ -33,12 +35,11 @@ function addToCart(codigo, nombre, id, precio) {
   document.querySelector(".carrito-contenido ul").append(carritoListLi);
 }
 
-
 // Una salida por consola para verificar el proceso
 console.log(productos);
 
-//Funcion que calcula el iva de los productos de forma individual
-// y luego recorre el array de productos para sumar uno a uno en una variable
+
+// Funcion que recorre el array de productos para sumar uno a uno en una variable
 // llamada total. Finalmente una salida por alert para el usuario y una por consola para el
 // programador.
 function comprar(productos) {
@@ -51,10 +52,18 @@ function comprar(productos) {
   alert(total);
 }
 
+//Funcion que vacia el array y tambien el html del carrito
+function vaciarCarro(productos) {
+  while (productos.length != 0) {
+    productos.pop();
+  }
+  carritoList.innerHTML = " ";
+  console.log(productos);
+}
 
-// Los botones
-
+// Los botones de cada item en la tienda
 let botonCompra = document.getElementById("botonCompra");
+let botonVaciarCarro = document.getElementById("botonVaciarCarro");
 let boton1 = document.getElementById("ateezFeverEpilogue");
 let boton2 = document.getElementById("ateezSpinOffFtW");
 let boton3 = document.getElementById("ateezTreasureEp3Wave");
@@ -64,12 +73,13 @@ let boton6 = document.getElementById("ateezFeverZeroP1");
 let boton7 = document.getElementById("ateezTreasureVol1");
 let boton8 = document.getElementById("ateezTreasureMapToAnswerSanv");
 
-botonCompra.onclick = () => { comprar(productos) }
-boton1.onclick = () => { addToCart("ateezFeverEpilogue", "Ateez Fever Epilogue", 1, 15000) }
-boton2.onclick = () => { addToCart("ateezSpinOffFtW", "Ateez Spin of from the Witness", 2, 10000) }
-boton3.onclick = () => { addToCart("ateezTreasureEp3Wave", "Ateez treasure ep3 wave ver", 3, 30000) }
-boton4.onclick = () => { addToCart("ateezTreasureEpilogue", "Ateez treasure epilogue", 4, 5000) }
-boton5.onclick = () => { addToCart("ateezBlackCatNero", "Ateez black cat nero lim. ed.", 5, 14000) }
-boton6.onclick = () => { addToCart("ateezFeverZeroP1", "Ateez fever zero: part 1", 6, 18200) }
-boton7.onclick = () => { addToCart("ateezTreasureVol1", "Ateez treasure vol. 1 all to action", 7, 3000) }
-boton8.onclick = () => { addToCart("ateezTreasureMapToAnswerSanv", 'Ateez treasure map to answer San ver.', 8, 10500) }
+botonCompra.onclick = () => { comprar(productos) };
+botonVaciarCarro.onclick = () => { vaciarCarro(productos); };
+boton1.onclick = () => { addToCart("ateezFeverEpilogue", "Ateez Fever Epilogue", 1, 15000) };
+boton2.onclick = () => { addToCart("ateezSpinOffFtW", "Ateez Spin of from the Witness", 2, 10000) };
+boton3.onclick = () => { addToCart("ateezTreasureEp3Wave", "Ateez treasure ep3 wave ver", 3, 30000) };
+boton4.onclick = () => { addToCart("ateezTreasureEpilogue", "Ateez treasure epilogue", 4, 5000) };
+boton5.onclick = () => { addToCart("ateezBlackCatNero", "Ateez black cat nero lim. ed.", 5, 14000) };
+boton6.onclick = () => { addToCart("ateezFeverZeroP1", "Ateez fever zero: part 1", 6, 18200) };
+boton7.onclick = () => { addToCart("ateezTreasureVol1", "Ateez treasure vol. 1 all to action", 7, 3000) };
+boton8.onclick = () => { addToCart("ateezTreasureMapToAnswerSanv", 'Ateez treasure map to answer San ver.', 8, 10500) };
