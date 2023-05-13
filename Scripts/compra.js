a532f9d279df3fabe2a2f0c01069ce2ba9c4b56c
Florencia Molle <floorimolle98@gmail.com>
2023-05-13 19:13:39 -0300

    Solucion de error de repeticion de html generado con el js y css para ese html

--- Scripts/compra.js
+++ Scripts/compra.js
@@ -1,50 +1,3 @@
-//script para elegir un numero de producto pero que tiene un error ya que al poner exit muestra el texto de "ingresaste numero incorrecto"
-/* function compra() {
-    let nombre = prompt("Buen día, por favor ingresa tu nombre a continuación");
-    let eleccion = prompt(nombre + " por favor, ingresa el numero de producto del 1 al 9");
-    if (eleccion > 9 || eleccion <= 0) {
-        alert("Ingresaste un número incorrecto");
-    } else {
-        do {
-          alert("El producto número " + eleccion + " fue agregado al carrito.");
-          eleccion = prompt(nombre + " por favor, ingrese otro numero o escriba Exit para finalizar.");
-          if (eleccion == "Exit") {
-            alert("Se agregaron todos los productos al carrito.");
-            break;
-          }
-        } while (eleccion <= 9 && eleccion > 0 && eleccion != null);
-         alert("Ingresaste un número incorrecto");
-    }
-} */
-
-/* function compra() {
-  const nombre = prompt("Buen día, por favor ingresa tu nombre a continuación");
-  let eleccion = prompt(
-    nombre + " por favor, ingresa el numero de producto del 1 al 9"
-  );
-  alert("El producto número " + eleccion + " fue agregado al carrito.");
-  flag = true;
-  if (eleccion <= 9 && eleccion > 0 && eleccion != null) {
-    while (flag) {
-      eleccion = prompt(
-        nombre +
-          " por favor, ingrese otro numero o escriba Exit para finalizar."
-      );
-      if (eleccion <= 9 && eleccion > 0 && eleccion != null) {
-        alert("El producto número " + eleccion + " fue agregado al carrito.");
-      } else if (eleccion == "Exit") {
-        flag = false;
-        alert("Se agregaron todos los productos al carrito.");
-      } else {
-        flag = false;
-        alert("Ingresaste un número incorrecto");
-      }
-    }
-  } else {
-    alert("Ingresaste un número incorrecto");
-  }
-} */
-
 let carritoHTML = document.getElementById("carrito");
 carritoHTML.innerHTML = "<p>Tu carrito</p><div></div>";
 document.querySelector("#tiendaFanPage_Fila header #navbar_Extended-Left").append(carritoHTML);
@@ -55,39 +8,30 @@ carritoInner.innerHTML = "<ul></ul>";
 
 let carritoList = document.querySelector("#tiendaFanPage_Fila header #navbar_Extended-Left #carrito .carrito-contenido ul");
 
-
 /* Al clickear en los botones + de cada procducto se añaden al carrito.
 Luego, al ir hasta la barra superior, al apretar el boton de la tarjeta de credito se suman 
 todos los productos seleccionados + su iva y luego se muestra el total por consola y por alert. */
 
 // Clase que crea mis productos cuando los sumo
 class Producto {
-    constructor(codigo, nombre, id, precio) {
-      this.codigo = codigo;
-      this.nombre = nombre;
-      this.id = id;
-      this.precio = parseFloat(precio);
-    }
-
-    sumaIva() {
-      this.precio = this.precio * 1.21;
-   }
+  constructor(codigo, nombre, id, precio) {
+    this.codigo = codigo;
+    this.nombre = nombre;
+    this.id = id;
+    this.precio = parseFloat(precio);
+  }
 }
 
 // Array y funcion que agrega los productos seleccionados a dicho array
-const productos = []
+const productos = [];
 function addToCart(codigo, nombre, id, precio) {
   productos.push(new Producto(codigo, nombre, id, precio));
-  for (const producto of productos) {
-    let carritoListLi = document.createElement("li");
-    carritoListLi.innerHTML = `<b> ${producto.nombre} </b> 
-                               <p> / ${producto.precio}</p>`;
-    document.querySelector("#tiendaFanPage_Fila header #navbar_Extended-Left #carrito .carrito-contenido ul").append(carritoListLi);
-  }
+  let producto = productos[productos.length-1];
+  let carritoListLi = document.createElement("li");
+  carritoListLi.innerHTML = `<b> ${producto.nombre} </b> 
+                             <p> ${producto.precio} / ${productos[productos.length - 1].precio *= 1.21}</p>`;
+  document.querySelector(".carrito-contenido ul").append(carritoListLi);
 }
-//Ya crea los li en donde tiene que, pero los loopea mal, agregando el nuevo + los viejos cuando solo
-// deberia agregar el nuevo
-
 
 
 // Una salida por consola para verificar el proceso
@@ -95,11 +39,10 @@ console.log(productos);
 
 //Funcion que calcula el iva de los productos de forma individual
 // y luego recorre el array de productos para sumar uno a uno en una variable
-// llamada total. Finalmente una salida por alert para el usuario y una por consola para el 
+// llamada total. Finalmente una salida por alert para el usuario y una por consola para el
 // programador.
 function comprar(productos) {
   let total = 0;
-  for (const producto of productos) producto.sumaIva();
   for (let i = 0; i < productos.length; i++) {
     total = total + productos[i].precio;
     console.log(productos[i].precio);
@@ -107,3 +50,26 @@ function comprar(productos) {
   console.log(total);
   alert(total);
 }
+
+
+// Los botones
+
+let botonCompra = document.getElementById("botonCompra");
+let boton1 = document.getElementById("ateezFeverEpilogue");
+let boton2 = document.getElementById("ateezSpinOffFtW");
+let boton3 = document.getElementById("ateezTreasureEp3Wave");
+let boton4 = document.getElementById("ateezTreasureEpilogue");
+let boton5 = document.getElementById("ateezBlackCatNero");
+let boton6 = document.getElementById("ateezFeverZeroP1");
+let boton7 = document.getElementById("ateezTreasureVol1");
+let boton8 = document.getElementById("ateezTreasureMapToAnswerSanv");
+
+botonCompra.onclick = () => { comprar(productos) }
+boton1.onclick = () => { addToCart("ateezFeverEpilogue", "Ateez Fever Epilogue", 1, 15000) }
+boton2.onclick = () => { addToCart("ateezSpinOffFtW", "Ateez Spin of from the Witness", 2, 10000) }
+boton3.onclick = () => { addToCart("ateezTreasureEp3Wave", "Ateez treasure ep3 wave ver", 3, 30000) }
+boton4.onclick = () => { addToCart("ateezTreasureEpilogue", "Ateez treasure epilogue", 4, 5000) }
+boton5.onclick = () => { addToCart("ateezBlackCatNero", "Ateez black cat nero lim. ed.", 5, 14000) }
+boton6.onclick = () => { addToCart("ateezFeverZeroP1", "Ateez fever zero: part 1", 6, 18200) }
+boton7.onclick = () => { addToCart("ateezTreasureVol1", "Ateez treasure vol. 1 all to action", 7, 3000) }
+boton8.onclick = () => { addToCart("ateezTreasureMapToAnswerSanv", 'Ateez treasure map to answer San ver.', 8, 10500) }
